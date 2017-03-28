94318982fb4171ccf92fb7064db96616369bc870
klocode <kendrick.t.lo@gmail.com>
2017-03-28 16:21:39 -0400

    added serializers

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,2 +1,14 @@
 class CategoriesController < ApplicationController
+
+  def index
+    @categories = Category.all
+    render json: @categories
+  end
+
+  def show
+    @category = Category.find_by(id: params[:id])
+    @items = @category.inventories
+    render json: @items
+  end
+
 end
