7db9452a2574cb65d084e5843141f0b3719e289a
SoriaMiguel <soria.miguel.8@gmail.com>
2017-03-29 15:34:15 -0400

    Create method in sessions_controller, login route to sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,3 +1,11 @@
 class SessionsController < ApplicationController
-  
+
+  def create
+    @user = User.find_by(username: params[:username]).try(:authenticate, params[:password])
+    if @user
+      render json: @user, serializer: UserExpandedSerializer
+    else
+      render json: ["Incorrect username and/or password.  Please try again."], status: 401
+    end
+  end
 end
