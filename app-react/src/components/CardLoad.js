06c190011fa6a61fe005c2b1863a7b977fe8d430
kelsey vaught <kelsey.vaught92@gmail.com>
2017-03-30 15:40:12 -0400

    additional css styling

--- app-react/src/components/CardLoad.js
+++ app-react/src/components/CardLoad.js
@@ -14,7 +14,17 @@ fetch('/users.json')
     return response.json()
   })
 
-
+fetch('https://mywebsite.com/endpoint/', {  
+  method: 'POST',
+  headers: {
+    'Accept': 'application/json',
+    'Content-Type': 'application/json',
+  },
+  body: JSON.stringify({
+    firstParam: 'yourValue',
+    secondParam: 'yourOtherValue',
+  })
+})
   componentDidMount() {
     fetch('/api/foo')
       .then(response => response.json())
