5c2f9abd826f1cd0d4bb3e61f1cb649aaee4ba0b
klocode <kendrick.t.lo@gmail.com>
2017-03-31 11:24:22 -0400

    added creating a shipping/address

--- app/models/shipping.rb
+++ app/models/shipping.rb
@@ -1,4 +1,6 @@
 class Shipping < ApplicationRecord
   belongs_to :user, optional: true
   has_many :orders
+
+  validates :first_name, :last_name, :street, :city, :state, :zip, :email, presence: true
 end
